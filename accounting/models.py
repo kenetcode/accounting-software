165f8efdb7cdaf1d0687b68775206ee1cc6c2ca2
kenetcode <k2ken2424@gmail.com>
2024-10-15 16:36:57 -0600

    Agregando la logia del login

--- accounting/models.py
+++ accounting/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+# Clase Usuario que hereda de models.Model para poder ser almacenada en la base de datos
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=255)
+    contrasena = models.CharField(max_length=255)
+
+    
--- accounting/urls.py
+++ accounting/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 app_name = 'accounting'
 urlpatterns = [
-    #path('login/', views.login, name='login'), # este path es para la vista de login modificar cuando ya se tenga la vista de login en el archivo views.py de la aplicacion accounting 
+    path('login/', views.login, name='login'),
+    path('', views.home_view, name='home'),
 ]
