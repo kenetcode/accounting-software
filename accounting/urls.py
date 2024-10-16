c9a52ed484e72a25940cf546728137e25588c3bb
kenetcode <k2ken2424@gmail.com>
2024-10-15 19:10:09 -0600

    Prueba de autenticacion

--- accounting/urls.py
+++ accounting/urls.py
@@ -4,4 +4,5 @@ app_name = 'accounting'
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('login/', views.login_view, name='login'),
+    path('prueba/', views.prueba_view, name='prueba'), #Aqui se define la ruta de la vista de prueba borrar despues
 ]
--- accounting/views.py
+++ accounting/views.py
@@ -8,11 +8,16 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 # Create your views here.
-@login_required
+
+
 def home_view(request):
     return render(request, 'home.html')
 
-@login_required
+@login_required 
+def prueba_view(request): #Aqui se define la vista de prueba borrar despues
+    return render(request, 'vistaDePrueba.html')
+
+
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
