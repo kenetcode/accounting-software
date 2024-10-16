e7c55517feb23002bd0202b06b6c14cd136172af
kenetcode <k2ken2424@gmail.com>
2024-10-15 18:01:46 -0600

    arreglando ruta home

--- accounting/urls.py
+++ accounting/urls.py
@@ -3,5 +3,5 @@ from . import views
 app_name = 'accounting'
 urlpatterns = [
     path('login/', views.login_view, name='login'),
-    path('home/', views.home_view, name='home'),
+    path('', views.home_view, name='home'),
 ]
