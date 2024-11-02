6fc115d400330da1807ca4290d0b022c731b2996
kenetcode <k2ken2424@gmail.com>
2024-11-01 23:30:12 -0600

    Fixed DB

--- accounting/urls.py
+++ accounting/urls.py
@@ -4,9 +4,9 @@ app_name = 'accounting'
 urlpatterns = [
     path('', views.home_view, name='home'),
     #Borrar despues solo servira de prueba
-    path('prueba/', views.prueba_view, name='prueba'),
-    path('cuentas/<int:id>/', views.cuentas_view, name='cuentas'),
+    #path('prueba/', views.prueba_view, name='prueba'),
+    #path('cuentas/<int:id>/', views.cuentas_view, name='cuentas'),
     #Borrar
     path('login/', views.login_view, name='login'),
-    path('catalogo/', views.catalogo_view, name='catalogo'),
+    #path('catalogo/', views.catalogo_view, name='catalogo'),
 ]
