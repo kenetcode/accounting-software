031b0fe50c4db134d760e25419032e8623e9db76
kenetcode <k2ken2424@gmail.com>
2024-11-02 10:40:11 -0600

    Script para las cuentas de la 14 a la 27

--- accounting/scripts/crear_cuentas.py
+++ accounting/scripts/crear_cuentas.py
@@ -32,13 +32,99 @@ def run():
  """
 def run():
 #Esta parte es de Carlos    
-print("Hola")
+    print("Hola")
 
 
 #--------------------------------------------------------------------------------
 #Esta parte es de Kenet
-print("Hola")
 
+cuenta_mayor14 = CuentasMayor.objects.create(
+        codigoCuenta="3101",
+        nombreCuenta="Capital social",
+        naturaleza="Capital"
+    )
 
+cuenta_mayor15 = CuentasMayor.objects.create(
+        codigoCuenta="3102",
+        nombreCuenta="Utilidad o Pérdida",
+        naturaleza="Capital"
+    )
 
+cuenta_mayor16 = CuentasMayor.objects.create(
+        codigoCuenta="4101",
+        nombreCuenta="Costo de venta de licencias",
+        naturaleza="Gastos y costos operativos"
+    )
 
+cuenta_mayor17 = CuentasMayor.objects.create(
+        codigoCuenta="4102",
+        nombreCuenta="Sueldos y salarios",
+        naturaleza="Gastos y costos operativos"
+    )
+
+cuenta_mayor18 = CuentasMayor.objects.create(
+        codigoCuenta="4103",
+        nombreCuenta="Vacaciones",
+        naturaleza="Gastos y costos operativos"
+    )
+
+cuenta_mayor19 = CuentasMayor.objects.create(
+        codigoCuenta="4104",
+        nombreCuenta="Aguinaldo",
+        naturaleza="Gastos y costos operativos"
+    )
+
+cuenta_mayor20 = CuentasMayor.objects.create(
+        codigoCuenta="4105",
+        nombreCuenta="Cuota patronal ISSS",
+        naturaleza="Gastos y costos operativos"
+    )
+
+cuenta_mayor21 = CuentasMayor.objects.create(
+        codigoCuenta="4106",
+        nombreCuenta="Cuota patronal AFP",
+        naturaleza="Gastos y costos operativos"
+    )
+
+cuenta_mayor22 = CuentasMayor.objects.create(
+        codigoCuenta="4107",
+        nombreCuenta="INCAF",
+        naturaleza="Gastos y costos operativos"
+    )
+
+cuenta_mayor23 = CuentasMayor.objects.create(
+        codigoCuenta="4108",
+        nombreCuenta="Gastos de mantenimiento de equipos",
+        naturaleza="Gastos y costos operativos"
+    )
+
+cuenta_mayor24 = CuentasMayor.objects.create(
+        codigoCuenta="4109",
+        nombreCuenta="Gastos de administración",
+        naturaleza="Gastos y costos operativos"
+    )
+
+cuenta_mayor25 = CuentasMayor.objects.create(
+        codigoCuenta="4110",
+        nombreCuenta="Gastos financieros",
+        naturaleza="Gastos y costos operativos"
+    )
+
+cuenta_mayor26 = CuentasMayor.objects.create(
+        codigoCuenta="5101",
+        nombreCuenta="Ingresos por venta de licencias de software",
+        naturaleza="Ingresos por ventas de software"
+    )
+
+cuenta_detalle = CuentasDetalle.objects.create(
+        codigoCuentaMayor=cuenta_mayor26,
+        codigoCuenta="510101",
+        nombreCuenta="Ingresos por venta directa de licencias",
+        naturaleza="Ingresos por ventas de software"
+    )
+
+cuenta_mayor27 = CuentasMayor.objects.create(
+        codigoCuenta="5102",
+        nombreCuenta="Ingresos por venta de servicios",
+        naturaleza="Ingresos por ventas de software"
+    )
