b88afdaf4ffb81a3c3d3bceb3b2163f9231b7b22
CarlosRauda64 <rauda.carlos6432@gmail.com>
2024-11-02 09:30:31 -0600

    :sparkles:Se añadio los linkeos a otras pantallas desde Home y SideBar

--- accounting/static/js/sidebar.js
+++ accounting/static/js/sidebar.js
@@ -2,6 +2,12 @@ const btnToggle = document.querySelector('.toggle-button');
 const btnHome = document.querySelector('.home-button');
 const btnCatalogo = document.querySelector('.catalogo-button');
 const btnTransacciones = document.querySelector('.transacciones-button');
+/* nuevos */
+const btnControlCostos = document.querySelector('.costos-button');
+const btnEstadosFinancieros = document.querySelector('.estados-button');
+const btnLibroMayor = document.querySelector('.libro-button');
+const btnCierreContable = document.querySelector('.cierre-button');
+
 let toggle = 0;
 
 btnToggle.addEventListener('click', function () {
@@ -30,13 +36,31 @@ btnHome.addEventListener('click', function () {
     window.location.href = "/";
 });
 
-//MODIFICAR
+//MODIFICAR (YA ESTA)
 btnTransacciones.addEventListener('click', function () {
-    window.location.href = "/prueba/";
+    window.location.href = "/transacciones/";
 });
 
 btnCatalogo.addEventListener('click', function () {
     window.location.href = "/catalogo/";
+});
+
+/* NUEVOS */
+
+btnControlCostos.addEventListener('click', function () {
+    window.location.href = "/controldecostos/";
+});
+
+btnEstadosFinancieros.addEventListener('click', function () {
+    window.location.href = "/estadosfinancieros/";
+});
+
+btnLibroMayor.addEventListener('click', function () {
+    window.location.href = "/libromayor/";
+});
+
+btnCierreContable.addEventListener('click', function () {
+    window.location.href = "/cierrecontable/";
 });
 
 
