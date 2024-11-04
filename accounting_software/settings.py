8c49cde7732ba97b45b85c3f71a7354cb3b79d63
kenetcode <k2ken2424@gmail.com>
2024-11-04 15:06:43 -0600

    Configuraciones completas para poder hacer deploy en render

--- accounting_software/settings.py
+++ accounting_software/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 #Importamos el módulo os
 import os
 from pathlib import Path
+#para el despliegue
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,13 +23,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7nb5(-37_4xarr-qik$b^@o36opf)!ocjwjlp!ub&cv^f3eolq'
+#SECRET_KEY = 'django-insecure-7nb5(-37_4xarr-qik$b^@o36opf)!ocjwjlp!ub&cv^f3eolq'
+
+#Para el despliegue en render
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
@@ -49,6 +60,7 @@ AUTHENTICATION_BACKENDS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -78,7 +90,15 @@ TEMPLATES = [
 # Definiendo la ruta de los archivos estáticos en STATIC_URL 
 STATIC_URL = '/static/'
 # Definiendo la ruta de los archivos estáticos de la aplicación 'accounting' en STATICFILES_DIRS
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'accounting/static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'accounting/static')]
+
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 WSGI_APPLICATION = 'accounting_software.wsgi.application'
 
@@ -87,10 +107,16 @@ WSGI_APPLICATION = 'accounting_software.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    """ 'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    } """
+
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 
---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r requirements.txt
+
+# Convert static asset files
+python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
+python manage.py migrate
+
+# Run the script to add data to the models
+python manage.py runscript crear_cuentas
+
+# Create a superuser if it doesn't exist
+python manage.py shell <<EOF
+import os
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@gmail.com')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin')
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+EOF
+
