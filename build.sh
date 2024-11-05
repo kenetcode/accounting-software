e66f08282cbe27c394a9778d7cff69ddd98f240f
kenetcode <k2ken2424@gmail.com>
2024-11-04 19:38:33 -0600

    fixed build.sh 2

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+# Apply any outstanding database migrations
+python manage.py migrate
+
 # Truncate all tables in the database
 python manage.py shell <<EOF
 from django.db import connection
@@ -14,9 +17,6 @@ EOF
 
 # Convert static asset files
 python manage.py collectstatic --no-input
-
-# Apply any outstanding database migrations
-python manage.py migrate
 
 # Run the script to add data to the models
 python manage.py runscript crear_cuentas
