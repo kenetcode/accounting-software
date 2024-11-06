f9c7933636f61f7408925f4781c98698edf9c478
kenetcode <k2ken2424@gmail.com>
2024-11-06 11:49:02 -0600

    Actualizando el .sh final scripts deploy

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ python manage.py makemigrations
 python manage.py migrate
 
 # Run the script to add data to the models
-#python manage.py runscript crear_cuentas
-#python manage.py runscript crear_departamentos
+python manage.py runscript crear_cuentas
+python manage.py runscript crear_departamentos
 
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@gmail.com', 'admin')"
+#python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@gmail.com', 'admin')"
