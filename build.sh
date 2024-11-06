64b675d83d3031b1b9613cd23693b4cbc5a6ef11
kenetcode <k2ken2424@gmail.com>
2024-11-05 23:22:31 -0600

    update script

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 #python manage.py makemigrations
-#python manage.py migrate
+python manage.py migrate
 
 
 # Borrar todos los datos de los modelos y restablecer los IDs
