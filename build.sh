6be123ac3b883adf15c6f573bcb9fd26712f71eb
kenetcode <k2ken2424@gmail.com>
2024-11-06 11:33:01 -0600

    Actualizando el .sh final

--- build.sh
+++ build.sh
@@ -9,21 +9,21 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-#python manage.py makemigrations
-#python manage.py migrate
+python manage.py makemigrations
+python manage.py migrate
 
 # Run the script to add data to the models
 #python manage.py runscript crear_cuentas
 #python manage.py runscript crear_departamentos
 
-# Create a superuser if it doesn't exist
-#python manage.py shell <<EOF
-#import os
-#from django.contrib.auth import get_user_model
-#User = get_user_model()
-#username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
-#email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@gmail.com')
-#password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin')
-#if not User.objects.filter(username=username).exists():
-#    User.objects.create_superuser(username, email, password)
-#EOF
+ Create a superuser if it doesn't exist
+python manage.py shell <<EOF
+import os
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@gmail.com')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin')
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+EOF
