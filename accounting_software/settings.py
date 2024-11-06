6020de130c2cffae26c15094e2c63f4ec20cfbdd
kenetcode <k2ken2424@gmail.com>
2024-11-06 00:48:03 -0600

    Preparando el deploy

--- accounting_software/settings.py
+++ accounting_software/settings.py
@@ -107,10 +107,11 @@ WSGI_APPLICATION = 'accounting_software.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 
