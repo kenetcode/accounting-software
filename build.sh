99f67e34b4faccef799478b101a2247677540387
kenetcode <k2ken2424@gmail.com>
2024-11-06 11:42:10 -0600

    Actualizando el .sh final 3

--- build.sh
+++ build.sh
@@ -16,14 +16,4 @@ python manage.py migrate
 #python manage.py runscript crear_cuentas
 #python manage.py runscript crear_departamentos
 
-Create a superuser if it doesn't exist
-python manage.py shell <<EOF
-import os
-from django.contrib.auth import get_user_model
-User = get_user_model()
-username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
-email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@gmail.com')
-password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin')
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
-EOF
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@gmail.com', 'admin')"
