861d87da90b329048078c1d7b2df4ed0f855336b
kenetcode <k2ken2424@gmail.com>
2024-11-06 00:54:35 -0600

    Preparando el deploy - data

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 #python manage.py makemigrations
-python manage.py migrate
+#python manage.py migrate
 
 # Run the script to add data to the models
 python manage.py runscript crear_cuentas
