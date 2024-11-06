c5cf8a08c5b0baff014e5486929106cc502ecad3
kenetcode <k2ken2424@gmail.com>
2024-11-05 20:56:43 -0600

    update .sh

--- build.sh
+++ build.sh
@@ -5,10 +5,6 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-# Create and apply migrations
-python manage.py makemigrations
-python manage.py migrate
-
 # Eliminar todas las tablas de la base de datos
 python manage.py shell <<EOF
 from django.db import connection
@@ -16,31 +12,24 @@ from django.apps import apps
 
 models = apps.get_models()
 with connection.cursor() as cursor:
+    cursor.execute("SET CONSTRAINTS ALL DEFERRED;")
     for model in models:
         table_name = model._meta.db_table
         cursor.execute(f"DROP TABLE IF EXISTS {table_name} CASCADE;")
 EOF
 
-# Create and apply migrations again after dropping tables
+# Crear y aplicar migraciones
 python manage.py makemigrations
 python manage.py migrate
 
-# Truncate all tables in the database
-python manage.py shell <<EOF
-from django.db import connection
-with connection.cursor() as cursor:
-    cursor.execute("TRUNCATE TABLE accounting_cuentasdetalle, accounting_cuentasmayor, accounting_transaccion, accounting_balancedecomprobacion, accounting_empleado RESTART IDENTITY CASCADE;")
-EOF
-
-# Convert static asset files
+# Convertir archivos de activos estáticos
 python manage.py collectstatic --no-input
 
-# Run the script to add data to the models
+# Ejecutar script para agregar datos a los modelos
 python manage.py runscript crear_cuentas
-
 python manage.py runscript crear_departamentos
 
-# Create a superuser if it doesn't exist
+# Crear un superusuario si no existe
 python manage.py shell <<EOF
 import os
 from django.contrib.auth import get_user_model
@@ -51,4 +40,3 @@ password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin')
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username, email, password)
 EOF
-
