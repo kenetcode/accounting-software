6acef00fcad9481b9a25a87f53e5c3f01d0c83d5
kenetcode <k2ken2424@gmail.com>
2024-11-05 22:47:11 -0600

    Update deploy final 2

--- accounting_software/settings.py
+++ accounting_software/settings.py
@@ -107,10 +107,6 @@ WSGI_APPLICATION = 'accounting_software.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-"""     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    } """
 
     #Para el despliegue en render
     'default': dj_database_url.config(
