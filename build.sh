582b79de46fc33478176a8404436e710669398d1
kenetcode <k2ken2424@gmail.com>
2024-11-06 00:40:35 -0600

    Data actualizada

--- build.sh
+++ build.sh
@@ -9,20 +9,8 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py makemigrations
+#python manage.py makemigrations
 python manage.py migrate
-
-# Borrar todos los datos de los modelos y restablecer los IDs
-python manage.py shell <<EOF
-from django.db import connection
-from accounting.models import CuentasMayor, CuentasDetalle, Transaccion, BalanceDeComprobacion, Departamento, Empleado, EstadoDeResultados
-
-models = [CuentasMayor, CuentasDetalle, Transaccion, BalanceDeComprobacion, Departamento, Empleado, EstadoDeResultados]
-with connection.cursor() as cursor:
-    for model in models:
-        table_name = model._meta.db_table
-        cursor.execute(f"TRUNCATE TABLE {table_name} RESTART IDENTITY CASCADE;")
-EOF
 
 # Run the script to add data to the models
 python manage.py runscript crear_cuentas
