f336a6b3498ede3af55cbfe8d43130a4851ae2fa
kenetcode <k2ken2424@gmail.com>
2024-11-05 21:00:50 -0600

    update 2

--- build.sh
+++ build.sh
@@ -5,31 +5,17 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-# Eliminar todas las tablas de la base de datos
-python manage.py shell <<EOF
-from django.db import connection
-from django.apps import apps
-
-models = apps.get_models()
-with connection.cursor() as cursor:
-    cursor.execute("SET CONSTRAINTS ALL DEFERRED;")
-    for model in models:
-        table_name = model._meta.db_table
-        cursor.execute(f"DROP TABLE IF EXISTS {table_name} CASCADE;")
-EOF
-
-# Crear y aplicar migraciones
-python manage.py makemigrations
-python manage.py migrate
-
-# Convertir archivos de activos estáticos
+# Convert static asset files
 python manage.py collectstatic --no-input
 
-# Ejecutar script para agregar datos a los modelos
-python manage.py runscript crear_cuentas
-python manage.py runscript crear_departamentos
+# Apply any outstanding database migrations
 
-# Crear un superusuario si no existe
+python manage.py migrate
+
+# Run the script to add data to the models
+python manage.py runscript crear_cuentas
+
+# Create a superuser if it doesn't exist
 python manage.py shell <<EOF
 import os
 from django.contrib.auth import get_user_model
